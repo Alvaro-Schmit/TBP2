835fa0da1dd3a333c65baa02f6291b4954d8375f
Alvaro Schmit <alvaroschmit@gmail.com>
2021-09-18 11:19:32 -0300

    iniciando git

---
+++ contrasena03.js
@@ -0,0 +1,44 @@
+
+function ir(event){
+  event.preventDefault();
+  let c = "1001";
+  if(document.form.password.value === c){
+    window.location="OnePage01.html";
+  }else{
+    alert("Contrasena incorreta");
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', function(event){
+  document.querySelector('form').addEventListener('submit', ir)
+})
+
+
+
+
+// let password = document.getElementById("password")
+// function ir(){
+// let c=108;
+//        if(password.value == c){
+//           window.location="OnePage.html";}
+//       else{
+//           alert('Contrasena incorreta');
+//       }
+// }
+
+// const entrada = document.getElementById('btn')
+
+// window.addEventListener('DOMContentLoaded', function(event){
+
+//   entrada.addEventListener('click', ir)
+// })
+
+// password.addEventListener("keypress", entrarConEnter(event))
+
+// function entrarConEnter(e){
+//   if(e.key == "Enter")
+//   ir()
+// }
+
+  
